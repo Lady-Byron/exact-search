e206ada799ad0394ad9580feaa6db82b95861122
Lady-Byron <samantha-everett@outlook.com>
2025-09-13 17:31:27 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -4,5 +4,6 @@ use Flarum\Extend;
 use LadyByron\ExactSearch\ExactScoutServiceProvider;
 
 return [
+    // 必须传“类名字符串”，不能传闭包
     (new Extend\ServiceProvider())->register(ExactScoutServiceProvider::class),
 ];
