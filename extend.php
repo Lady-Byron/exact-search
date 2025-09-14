855c694f0b1bb7b89cc85afb997036f9062b7be9
Lady-Byron <samantha-everett@outlook.com>
2025-09-13 22:33:43 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -2,8 +2,13 @@
 
 use Flarum\Extend;
 use LadyByron\ExactSearch\ExactScoutServiceProvider;
+use LadyByron\ExactSearch\TitleFirstDiscussionGambit;
+use Flarum\Discussion\Search\DiscussionSearcher;
 
 return [
-    // 必须传“类名字符串”，不能传闭包
     (new Extend\ServiceProvider())->register(ExactScoutServiceProvider::class),
+
+    // 让“讨论搜索”的全文解析使用我们自定义的合并策略（标题优先，其次正文）
+    (new Extend\SimpleFlarumSearch(DiscussionSearcher::class))
+        ->setFullTextGambit(TitleFirstDiscussionGambit::class),
 ];
